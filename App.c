4219e7f03f67a314150ede60bb21a1fd9f7a29b1
Ronald Fernandez <Rferna214@gmail.com>
2022-10-16 20:07:45 -0400

    10/16/22

--- App.c
+++ App.c
@@ -3,15 +3,24 @@
 // include header file
 #include "./headers/BasicIO.h"
 #include "./headers/Decisions.h"
+#include "./headers/Loops.h"
 
 int main(void){
     //puts("Hello World!!!!");
+    // Testing Basic IO functions
     //output();
     //input();
+    // Testing Decisions Functionality
     // decisions1();
     // decisions2();
     // decisions3();
     // decisions4();
     // decisions5();
-     decisions6();
+    // decisions6();
+    // Testing Loops functionality
+    loops1();
+    loops2();
+    loops3();
+    loops4();
+    loops5();
 }
---
+++ Loops.c
@@ -0,0 +1,82 @@
+// include pre-processor directive
+#include "./headers/Loops.h"
+
+/* This function will use a while loop*/
+void loops1(void){
+    // declare loop counter variable and initialize it
+    int i = START;
+
+    // set up while loop
+    // use macro expression as condition of while loop
+    while(condition(i)){
+        printf("%d ", i);
+        i--;
+    }
+    printf("blastoff\n");
+}
+
+/* This function will use a do-while loop*/
+void loops2(void){
+// declare loop counter variable and initialize it
+    int i = START;
+
+    // set up do-while loop
+        do {
+        printf("%d ", i);
+        i--;
+    } while(condition(i));// use macro expression as condition of while loop
+    printf("blastoff\n");
+}
+
+/* This function used a for loop*/
+void loops3(void){
+    //declare loop counter variable
+    int i;
+
+    // set up for loop
+    // use macro expression as condition of  for loop
+    for(i = START; condition(i); i--){
+        printf("%d ", i);
+    }
+    printf("blastoff\n");
+
+}
+
+/* This function uses a break statement to terminate a loop*/
+void loops4(void){
+    //declare loop counter variable
+    int i;
+
+    // set up for loop
+    // use macro expression as condition of  for loop
+    for(i = START; condition(i); i--){
+        printf("%d ", i);
+        // use macro expression as condition of if statement
+        if(break(i)){
+            // terminate loop
+            break;
+        }
+    }
+    printf("early blastoff\n");
+
+}
+
+/* this function uses a continue statement to skip the current
+* iteration of a loop */
+void loops5(void){
+//declare loop counter variable
+    int i;
+
+    // set up for loop
+    // use macro expression as condition of  for loop
+    for(i = START; condition(i); i--){
+        
+        // use macro expression as condition of if statement
+        if(continue(i) !=0){
+            // skip current iteration of loop
+            continue;
+        }
+        printf("%d ", i);
+    }
+    printf("even blastoff\n");
+}
